ea15e4256f32515f7001adda97b1653e3f4945d4
Edwin Ng'ang'a <moyiedwin8@gmail.com>
2024-07-04 11:20:20 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names=("Guadalupe", "Ollie", "Aki")
+
+function writeCards(names,event){
+    let loopMessages=[];
+    for(let i=0; i <names.length; i++) {
+      loopMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return loopMessages;
+}
+
+function countDown(){
+    let count=11;
+    while(count>0){
+        console.log(`${count}`)
+        count--
+    }
+}
